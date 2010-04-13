939d2625cfaf271a5b348aad5ce1d23cf72591ce
sverde1 <sverde1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-13 15:01:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/watermark-reloaded/trunk@228416 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 jQuery(document).ready(function() {
-	// hide color input field and show ColorPicker window
-	jQuery('#watermark_text_color_hash').hide();
-	jQuery('input[name="watermark_text[color]"]').hide();
+	// preview update trigger
+	updatePreview = function() {
+		jQuery('#previewImg_text').show();
+
+		jQuery('#previewImg_text').attr('src', location.href + '&watermarkPreview&' + unescape(jQuery('#watermark_text input, #watermark_text select').serialize()));
+	}
+	jQuery('#watermark_text input:text').keyup(updatePreview);
+	jQuery('#watermark_text select').change(updatePreview);
+	
+	// show color selector
 	jQuery('.colorSelector').show();
+	// define color selector events
 	jQuery('.colorSelector').ColorPicker({
 		color: jQuery('input[name="watermark_text[color]"]').val(),
 		onSubmit: function(hsb, hex, rgb, el) {
 			jQuery('.colorSelector div').css('background-color', '#' + hex);
 			jQuery('input[name="watermark_text[color]"]').val(hex);
 			jQuery(el).ColorPickerHide();
+			updatePreview();
 		}	
 	});
+	
+	updatePreview();
 });
